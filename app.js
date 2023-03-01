77b1082d493bf4caa49d5e25669e13055ce46c21
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2023-03-01 16:00:14 +0100

    Updated

--- app.js
+++ app.js
@@ -1,98 +1,46 @@
-// app.js
-window.onload = () => {
-	const form1 = document.querySelector("#addForm");
+const textarea = document.querySelector('.textarea');
+const addButton = document.querySelector('.buttoninput');
+const todoList = document.querySelector('.todolist');
 
-	let items = document.getElementById("items");
-	let submit = document.getElementById("submit");
-
-	let editItem = null;
-
-	form1.addEventListener("submit", addItem);
-	items.addEventListener("click", removeItem);
-};
-
-function addItem(e) {
-	e.preventDefault();
-
-	if (submit.value != "Submit") {
-		console.log("Hello");
-
-		editItem.target.parentNode.childNodes[0].data
-			= document.getElementById("item").value;
-
-		submit.value = "Submit";
-		document.getElementById("item").value = "";
-
-		document.getElementById("lblsuccess").innerHTML
-			= "Text edited successfully";
-
-		document.getElementById("lblsuccess")
-						.style.display = "block";
-
-		setTimeout(function() {
-			document.getElementById("lblsuccess")
-							.style.display = "none";
-		}, 3000);
-
-		return false;
-	}
-
-	let newItem = document.getElementById("item").value;
-	if (newItem.trim() == "" || newItem.trim() == null)
-		return false;
-	else
-		document.getElementById("item").value = "";
-
-	let li = document.createElement("li");
-	li.className = "list-group-item";
-
-	let deleteButton = document.createElement("button");
-
-	deleteButton.className =
-		"btn-danger btn btn-sm float-right delete";
-
-	deleteButton.appendChild(document.createTextNode("Delete"));
-
-	let editButton = document.createElement("button");
-
-	editButton.className =
-			"btn-success btn btn-sm float-right edit";
-
-	editButton.appendChild(document.createTextNode("Edit"));
-
-	li.appendChild(document.createTextNode(newItem));
-	li.appendChild(deleteButton);
-	li.appendChild(editButton);
-
-	items.appendChild(li);
-}
-
-function removeItem(e) {
-	e.preventDefault();
-	if (e.target.classList.contains("delete")) {
-		if (confirm("Are you Sure?")) {
-			let li = e.target.parentNode;
-			items.removeChild(li);
-			document.getElementById("lblsuccess").innerHTML
-				= "Text deleted successfully";
-
-			document.getElementById("lblsuccess")
-						.style.display = "block";
-
-			setTimeout(function() {
-				document.getElementById("lblsuccess")
-						.style.display = "none";
-			}, 3000);
-		}
-	}
-	if (e.target.classList.contains("edit")) {
-		document.getElementById("item").value =
-			e.target.parentNode.childNodes[0].data;
-		submit.value = "EDIT";
-		editItem = e;
-	}
-}
-
-function toggleButton(ref, btnID) {
-	document.getElementById(btnID).disabled = false;
-}
+addButton.addEventListener('click', () => {
+    // Create a new task/item element
+    const task = document.createElement('div');
+    task.classList.add('task');
+    const taskText = document.createElement('span');
+    taskText.innerText = textarea.value;
+    task.appendChild(taskText);
+    // Add edit and delete buttons to the task
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+    task.appendChild(editButton);
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    task.appendChild(deleteButton);
+    // Add the task to the to-do list
+    todoList.appendChild(task);
+    // Clear the input field
+    textarea.value = '';
+});
+todoList.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.tagName === 'BUTTON') {
+        const task = target.parentElement;
+        if (target.innerText === 'Edit') {
+            // Replace the task text with an input field for editing
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = task.querySelector('span').innerText;
+            task.replaceChild(input, task.querySelector('span'));
+            target.innerText = 'Save';
+        } else if (target.innerText === 'Save') {
+            // Replace the input field with the edited task text
+            const span = document.createElement('span');
+            span.innerText = target.previousSibling.value;
+            task.replaceChild(span, target.previousSibling);
+            target.innerText = 'Edit';
+        } else if (target.innerText === 'Delete') {
+            // Remove the task from the to-do list
+            todoList.removeChild(task);
+        }
+    }
+});
